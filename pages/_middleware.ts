6eabb7db6ffd44bd093ffcbb134b7c4366d2b168
Philip Ziolkowski <philipz4848@gmail.com>
2022-08-06 16:02:30 -0700

    Adding middleware edge function to check if the request even has a token

---
+++ pages/_middleware.ts
@@ -0,0 +1,15 @@
+// Does not run in a Node environement!
+import { NextResponse } from 'next/server'
+
+const signedInPages = ['/', '/playlist', '/library']
+
+// runs before any page request
+export default function middleware(req) {
+    if (signedInPages.find((p) => p === req.nextUrl.pathname)) {
+        const token = req.cookies.WAVES_ACCESS_TOKEN
+
+        if (!token) {
+            return NextResponse.redirect('/signin')
+        }
+    }
+}

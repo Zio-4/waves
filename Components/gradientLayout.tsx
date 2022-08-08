46a531b8091f1596316b96103d2d4c9b24963e03
Philip Ziolkowski <philipz4848@gmail.com>
2022-08-08 10:10:49 -0700

    Adding design for profile page

--- Components/gradientLayout.tsx
+++ Components/gradientLayout.tsx
@@ -8,7 +8,21 @@ const GradientLayout = ({color, children, image, subtitle, title, description, r
             <Box padding="20px">
                 <Image boxSize="160px" boxShadow="2xl" src={image} borderRadius={roundImage ? '100%' : '3px'}/>
             </Box>
+            <Box padding="20px" lineHeight="40px" color="white">
+              <Text fontSize="x-small" fontWeight="bold" casing="uppercase">
+                {subtitle}
+              </Text>
+              <Text fontSize="6xl">
+                {title}
+              </Text>
+              <Text fontSize="x-small" >
+                {description}
+              </Text>
+            </Box>
         </Flex>
+        <Box paddingY="50px">
+          {children}
+        </Box>
     </Box>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,42 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Box, Flex, Text } from '@chakra-ui/layout'
+import { Image } from '@chakra-ui/react'
 import GradientLayout from '../Components/gradientLayout'
-import styles from '../styles/Home.module.css'
+import prisma from '../lib/prisma'
 
-export default function Home() {
+function Home({ artists }) {
   return (
-    <GradientLayout color='red' roundImage>
+    <GradientLayout color='purple' subtitle="profile" title="Philip Ziolkowski" description="15 public playlists" image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2" roundImage>
       <div>Home</div>
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">Top artist this month</Text>
+          <Text fontSize="md">Only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image src="https://placekitten.com/300/300" borderRadius="100%"/>
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   )
 }
+
+export const getServerSideProps = async () => {
+  console.log('get Server side call')
+  const artists = await prisma.artist.findMany()
+  console.log('Artists: ', artists)
+  return {
+    props: { artists }
+  }
+}
+
+export default Home

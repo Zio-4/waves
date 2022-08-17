dd9d7eb224b68e83e2505b3f9bf0444144cd8b01
Philip Ziolkowski <philipz4848@gmail.com>
2022-08-17 14:59:21 -0700

    Adding loading states to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,51 @@
-import { Box, Text, Flex } from '@chakra-ui/layout'
-import { Image } from '@chakra-ui/react'
+import { Box, Flex, Text } from '@chakra-ui/layout'
+import { Image, SkeletonCircle, SkeletonText } from '@chakra-ui/react'
 import GradientLayout from '../Components/gradientLayout'
 import { useMe } from '../lib/hooks'
 import prisma from '../lib/prisma'
+import { useEffect, useState } from 'react'
 
-const Home = ({ artists }) => {
-  const { user } = useMe()
+ 
+function Home({ artists, image }) {
+  const [userIsLoaded, setUserIsLoaded] = useState(false)
+  const { user, isLoading } = useMe()
+
+  useEffect(() => {
+    if (!isLoading) {
+      setUserIsLoaded(true)
+    }
+  }, [isLoading])
+
 
   return (
-    <GradientLayout
-      roundImage
-      color="gray"
+    <GradientLayout 
+      color='gray' 
       subtitle="profile"
-      title={`${user?.firstName} ${user?.lastName}`}
-      description={`${user?.playlistsCount} public playlists`}
-      image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
+      title={`${user?.firstName} ${user?.lastName}`} 
+      description={`${user?.playlistsCount - 1} public playlists`}
+      image={image}
+      roundImage
     >
-      <Box color="white" paddingX="40px">
-        <Box marginBottom="40px">
-          <Text fontSize="2xl" fontWeight="bold">
-            Top artist this month
-          </Text>
-          <Text fontSize="md">only visible to you</Text>
+
+      <Box color="white" paddingX="2.5rem">
+        <Box marginBottom="1rem" marginLeft=".6rem">
+          <Text fontSize="2xl" fontWeight="bold">Top artist this month</Text>
+          <Box marginTop='1rem'>
+            <Text fontSize="md">Only visible to you</Text>
+          </Box>
         </Box>
         <Flex>
           {artists.map((artist) => (
-            <Box paddingX="10px" width="20%">
-              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
-                <Image
-                  src="https://placekitten.com/300/300"
-                  borderRadius="100%"
-                />
+            <Box paddingX=".6rem" width="20%" key={artist.name}>
+              <Box bg="gray.900" borderRadius="4px" padding="1.2rem" width="100%">
+                <SkeletonCircle size='100%' isLoaded={userIsLoaded}>
+                  <Image src="https://placekitten.com/300/300" borderRadius="100%"/>
+                </SkeletonCircle>
                 <Box marginTop="20px">
-                  <Text fontSize="large">{artist.name}</Text>
-                  <Text fontSize="x-small">Artist</Text>
+                  <SkeletonText isLoaded={userIsLoaded}>
+                    <Text fontSize="large">{artist.name}</Text>
+                    <Text fontSize="small">Artist</Text>
+                  </SkeletonText>
                 </Box>
               </Box>
             </Box>
@@ -45,10 +57,13 @@ const Home = ({ artists }) => {
 }
 
 export const getServerSideProps = async () => {
-  const artists = await prisma.artist.findMany({})
+  const artists = await prisma.artist.findMany()
+
+  const imageId = Math.floor(Math.random() * 1000)
+  const image = `https://picsum.photos/400?random=${imageId}`
 
   return {
-    props: { artists },
+    props: { artists, image }
   }
 }
 

7730f971f254b2ca8c8874bbb851c98377ee4e6a
Philip Ziolkowski <philipz4848@gmail.com>
2022-08-17 00:26:06 -0700

    sidebar

--- Components/sidebar.tsx
+++ Components/sidebar.tsx
@@ -4,9 +4,9 @@ import Image from "next/image"
 import Link from "next/link"
 import { usePlaylist } from "../lib/hooks"
 import logo from '../public/logo/wave-logo.svg'
-import { Skeleton, SkeletonCircle, SkeletonText, Spinner } from '@chakra-ui/react'
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
+// import { Skeleton, SkeletonCircle, SkeletonText, Spinner } from '@chakra-ui/react'
+// import { useEffect, useState } from 'react'
+// import { useRouter } from 'next/router'
 
 const navMenu = [
     { name: 'Home', icon: MdHome, route: '/' },
@@ -22,105 +22,82 @@ const musicMenu = [
 // Refactor ListItems into sepearte component
 
 const Sidebar = () => {
-    const { playlists, isLoading } = usePlaylist()
-    const [playlistLoaded, setPlaylistLoaded] = useState(false)
-    const [pageLoading, setPageLoading] = useState(false)
-    const router = useRouter()
-    const { id } = router.query
-
-
-    useEffect(() => {
-        if (!isLoading) {
-            setPlaylistLoaded(true)
-        }
-    }, [isLoading])
-
-    useEffect(() => {
-        return () => setPageLoading(false)
-    }, [])
-
+    const { playlists } = usePlaylist()
     return (
-        <Box width="100%" height="calc(100vh - 100px)" bg="black" paddingX="5px" color="gray">
-            <Box paddingY="20px" height="100%">
-                <Box width="120px" marginBottom="20px" paddingX="20px">
-                    <Image src={logo} height={60} width={120}/>
-                </Box>
-                <Box marginBottom="20px">
-                    <List spacing={2}>
-                        {navMenu.map(menu => (
-                            <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
-                                <LinkBox>
-                                    <Link href={menu.route} passHref>
-                                        <LinkOverlay color={id === undefined && menu.name === 'Home' ? 'white' : 'gray'}>
-                                            <ListIcon as={menu.icon} color="white" marginRight="20px" />
-                                            {menu.name}
-                                        </LinkOverlay>
-                                    </Link>
-                                </LinkBox>
-                            </ListItem>
-                        ))}
-                    </List>
-                </Box>
-
-                <Box marginTop="20px">
-                    <List spacing={2}>
-                        {musicMenu.map(menu => (
-                            <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
-                                <LinkBox>
-                                    <Link href={menu.route} passHref>
-                                        <LinkOverlay>
-                                            <ListIcon as={menu.icon} color="white" marginRight="20px" />
-                                            {menu.name}
-                                        </LinkOverlay>
-                                    </Link>
-                                </LinkBox>      
-                            </ListItem>
-                        ))}
-                    </List>
-                </Box>
-                <Divider color="gray.800"/>
-
-                <Box height="66%" overflowY="auto" paddingY="20px">
-                    <Skeleton />
-
-                    <List spacing={2}>
-                    
-                        {playlistLoaded && playlists.map((playlist) => (
-                                <ListItem paddingX="20px" key={playlist.id}>
-                                    <LinkBox>
-                                            <Link href={{
-                                                pathname: '/playlist/[id]',
-                                                query: { id: playlist.id }
-                                                }}
-                                                passHref
-                                            >
-                                                <LinkOverlay color={playlist.id === id ? 'white' : 'gray'}>
-                                                    {/* {playlist.id !== 7 ? playlist.name : null} */}
-                                                    {playlist.name}
-                                                </LinkOverlay>
-                                            </Link>
-                                    </LinkBox>
-                                </ListItem>
-                        ))}
-
-                        {!playlistLoaded && (
-                            <Stack>
-                                <Skeleton >1</Skeleton>
-                                <Skeleton >1</Skeleton>
-                                <Skeleton >1</Skeleton>
-                                <Skeleton >1</Skeleton>
-                                <Skeleton >1</Skeleton>
-                                <Skeleton >1</Skeleton>
-                                <Skeleton >1</Skeleton>
-                                <Skeleton >1</Skeleton>
-                                <Skeleton >1</Skeleton>
-                            </Stack>
-                        )}
-                    </List>
-                </Box>
-            </Box>
+      <Box
+        width="100%"
+        height="calc(100vh - 100px)"
+        bg="black"
+        paddingX="5px"
+        color="gray"
+      >
+        <Box paddingY="20px" height="100%">
+          <Box width="120px" marginBottom="20px" paddingX="20px">
+            <Image src={logo} height={60} width={120} />
+          </Box>
+          <Box marginBottom="20px">
+            <List spacing={2}>
+              {navMenu.map((menu) => (
+                <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                  <LinkBox>
+                    <Link href={menu.route} passHref>
+                      <LinkOverlay>
+                        <ListIcon
+                          as={menu.icon}
+                          color="white"
+                          marginRight="20px"
+                        />
+                        {menu.name}
+                      </LinkOverlay>
+                    </Link>
+                  </LinkBox>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+          <Box marginTop="20px">
+            <List spacing={2}>
+              {musicMenu.map((menu) => (
+                <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+                  <LinkBox>
+                    <Link href={menu.route} passHref>
+                      <LinkOverlay>
+                        <ListIcon
+                          as={menu.icon}
+                          color="white"
+                          marginRight="20px"
+                        />
+                        {menu.name}
+                      </LinkOverlay>
+                    </Link>
+                  </LinkBox>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+          <Divider color="gray.800" />
+          <Box height="66%" overflowY="auto" paddingY="20px">
+            <List spacing={2}>
+              {playlists.map((playlist) => (
+                <ListItem paddingX="20px" key={playlist.id}>
+                  <LinkBox>
+                    <Link
+                      href={{
+                        pathname: '/playlist/[id]',
+                        query: { id: playlist.id },
+                      }}
+                      passHref
+                    >
+                      <LinkOverlay>{playlist.name}</LinkOverlay>
+                    </Link>
+                  </LinkBox>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
         </Box>
+      </Box>
     )
-}
+  }
 
 export default Sidebar

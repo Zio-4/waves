052ff35925af70ccd4914081d53a0d1a12d2d6b7
Philip Ziolkowski <philipz4848@gmail.com>
2022-08-17 14:51:50 -0700

    Adding loading skeletons to gradient layout

--- Components/gradientLayout.tsx
+++ Components/gradientLayout.tsx
@@ -1,5 +1,8 @@
 import { Box, Flex, Text } from '@chakra-ui/layout'
-import { Image } from '@chakra-ui/react'
+import { Image, Skeleton, SkeletonCircle, Stack} from '@chakra-ui/react'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 
 const GradientLayout = ({
   color,
@@ -10,30 +13,57 @@ const GradientLayout = ({
   description,
   roundImage,
 }) => {
+  const [userIsLoaded, setUserIsLoaded] = useState(false)
+  const router = useRouter()
+  const { id } = router.query
+
+  useEffect(() => {
+    if (!title.includes('undefined')) {
+      setUserIsLoaded(true)
+    }
+  }, [title])
+
+
+
   return (
-    <Box
-      height="100%"
-      overflowY="auto"
-      bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.95) 75%)`}
-    >
-      <Flex bg={`${color}.600`} padding="40px" align="end">
-        <Box padding="20px">
-          <Image
-            boxSize="160px"
-            boxShadow="2xl"
-            src={image}
-            borderRadius={roundImage ? '100%' : '3px'}
-          />
+    <Box height="100%" overflowY="auto" bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.95) 75%)`}>
+        <Flex bg={`${color}.600`} padding="40px" align="end">
+
+              <Box padding="20px">
+                <SkeletonCircle size='40' isLoaded={userIsLoaded}>
+                  <Image boxSize="160px" boxShadow="2xl" src={image} borderRadius={roundImage ? '100%' : '3px'}/>
+                </SkeletonCircle>
+              </Box>
+
+              <Box padding="20px" lineHeight="40px" color="white">
+                <Stack spacing={1}>
+                  <Skeleton isLoaded={userIsLoaded}>
+                    <Text fontSize="small" fontWeight="bold" casing="uppercase">
+                      {subtitle}
+                    </Text>
+                  </Skeleton>
+                  
+                  <Skeleton isLoaded={userIsLoaded}>
+                    <Text fontSize="6xl" fontWeight='extrabold' marginTop='none'>
+                      {title.includes('Playlist') ? (
+                        `Playlist ${id}`
+                      ) : (
+                        title
+                      ) }
+                    </Text>
+                  </Skeleton>
+                  
+                  <Skeleton isLoaded={userIsLoaded}>
+                    <Text fontSize="small" >
+                      {description}
+                    </Text>
+                  </Skeleton>
+                </Stack>
+              </Box>
+        </Flex>
+        <Box paddingY="50px">
+          {children}
         </Box>
-        <Box padding="20px" lineHeight="40px" color="white">
-          <Text fontSize="x-small" fontWeight="bold" casing="uppercase">
-            {subtitle}
-          </Text>
-          <Text fontSize="6xl">{title}</Text>
-          <Text fontSize="x-small">{description}</Text>
-        </Box>
-      </Flex>
-      <Box paddingY="50px">{children}</Box>
     </Box>
   )
 }

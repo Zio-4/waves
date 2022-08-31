7bea536a8a1fe6c60b6f38b928ff722b4e816dd1
Philip Ziolkowski <philipz4848@gmail.com>
2022-08-30 19:22:28 -0700

    Adding API route for favorites, sub routes to add a song to favorites and remove from favorites. Adding favorites field to User model. Also adding store state for favorites

--- lib/store.ts
+++ lib/store.ts
@@ -3,6 +3,16 @@ import { createStore, action } from 'easy-peasy'
 export const store = createStore({
   activeSongs: [],
   activeSong: null,
+  favoriteSongs: [],
+  setFavoriteSongs: action((state: any, payload) => {
+    state.favoriteSongs = payload
+  }),
+  addToFavorites: action((state: any, payload) => {
+    state.favoriteSongs.push(payload)
+  }),
+  removeFromFavorites: action((state: any, payload) => {
+    state.favoriteSongs = state.favoriteSongs.filter(song => song !== payload)
+  }),
   changeActiveSongs: action((state: any, payload) => {
     state.activeSongs = payload
   }),
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,6 +36,7 @@ const theme = extendTheme({
 })
 
 function MyApp({ Component, pageProps }) {
+
   return (
     <ChakraProvider theme={theme}>
       <StoreProvider store={store}>

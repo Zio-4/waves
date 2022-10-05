3a87ea65ce968a5cb1395f3e4b462c0183959c56
Philip Ziolkowski <philipz4848@gmail.com>
2022-10-04 23:50:47 -0700

    Adding search functionality, loading spinner, and fixing design for search result cards

--- Components/searchLayout.tsx
+++ Components/searchLayout.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { Box, Heading, Flex } from '@chakra-ui/layout'
-import { Input, Grid, GridItem } from '@chakra-ui/react'
+import { Input, Grid, GridItem, Spinner } from '@chakra-ui/react'
 import SongSearchCard from './songSearchCard'
-
-const songs = ['test', 'another', 'test', 'okay']
 
 
 const SearchLayout = ({children, color}) => {
   const [allSongs, setAllSongs] = useState([])
+  const [searchInput, setSearchInput] = useState('')
+  const [filteredSongs, setFilteredSongs] = useState([])
 
   useEffect(() => {
     fetch('/api/songs', {
@@ -20,18 +20,35 @@ const SearchLayout = ({children, color}) => {
   }, [])
 
 
+
+  const updateAndParseSearchInput = (e) => {
+    setSearchInput(e.target.value)
+
+    const filtered = allSongs.filter(song => song.name.toLowerCase().includes(searchInput.toLowerCase()))
+
+    setFilteredSongs(filtered)
+
+  }
+
+  useEffect(() => {
+    if (searchInput.length == 0) {
+      setFilteredSongs([])
+    }
+  }, [searchInput])
+
+
   return (
     <Box height="100%" overflowY="auto" bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.95) 75%)`}>
       <Heading color='white' textAlign='center' paddingTop='2rem'>Search for songs to listen to</Heading>
 
       <Flex justifyContent='center'>
         <Box width='80%' paddingTop='2rem'>
-          <Input placeholder='Ex. Ghibli Chillhop'/>
+          <Input placeholder='Ex. Ghibli Chillhop' value={searchInput} onChange={updateAndParseSearchInput} textColor='white'/>
         </Box>
       </Flex>
 
       <Box width='80%' margin='auto' paddingTop='1.5rem'>
-        {allSongs.map((song) => {
+        {allSongs.length ? (filteredSongs.map((song) => {
           return <SongSearchCard id={song.id} 
                   artistID={song.artist.id} 
                   artistName={song.artist.name} 
@@ -41,10 +58,13 @@ const SearchLayout = ({children, color}) => {
                   url={song.url}
                   key={song.id}
                 />
-        })}
-
-
+        })) : null}
       </Box>
+
+      <Flex justifyContent='center'>
+        {searchInput.length > 0 && allSongs.length === 0 ? <Spinner size='xl'/> : null}
+      </Flex>
+
     </Box>
   )
 }

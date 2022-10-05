41e95678797f25aa41682d228f01c4b60f388f7b
Philip Ziolkowski <philipz4848@gmail.com>
2022-10-04 23:50:47 -0700

    Adding search functionality, loading spinner, and fixing design for search result cards

--- Components/songSearchCard.tsx
+++ Components/songSearchCard.tsx
@@ -2,32 +2,44 @@ import React from 'react'
 import { Box } from '@chakra-ui/layout'
 import { Grid, GridItem } from '@chakra-ui/layout'
 import Image from 'next/image'
+import { formatTime } from '../lib/formatters'
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
+import { BsThreeDotsVertical } from 'react-icons/bs'
 
 const SongSearchCard = ({id, artistID, artistName, duration, image, songName, url,}) => {
 
-    console.log(image)
-
   return (
-        <Box borderStyle='solid' borderWidth='thin' borderColor='black' bg='black' borderRadius='md' padding='1rem' marginY='1rem'>
-          <Grid
-              h='3rem'
-              templateRows='repeat(2, 1fr)'
-              templateColumns='repeat(6, 1fr)'
-              gap={1}
-            >
-              <GridItem rowSpan={2} bg='tomato' colSpan={1} colEnd={1}  color='white'>
-                <Image src={image} width={100} height={100}/>
-              </GridItem>
-              <GridItem colSpan={2}  color='white'>
-                {songName}
-              </GridItem>
-              <GridItem colSpan={4}  color='gray'>
-                {artistName}
-              </GridItem>
-              <GridItem colSpan={1} colStart={6} color='gray'> 
-                {duration}
-              </GridItem>
-          </Grid>
+        <Box  marginY='1rem'>
+            <Box borderStyle='solid' borderWidth='thin' borderColor='black' bg='black' borderRadius='md' height='6rem'>
+                <Grid
+                    h='2.5rem'
+                    templateRows='repeat(3, 1fr)'
+                    templateColumns='repeat(7, 1fr)'
+                    gap={2}
+                    marginBottom='1rem'
+                >
+                    <GridItem rowSpan={2}  colSpan={1} colEnd={1}  color='white'>
+                        <Image src={image} width={94} height={94}/>
+                    </GridItem>
+                    <GridItem padding='.8rem'></GridItem>
+                    <GridItem colSpan={4} rowStart={2} rowSpan={1} colStart={1} colEnd={4}  color='white'>
+                        {songName}
+                    </GridItem>
+                    <GridItem colSpan={4} rowStart={3} colStart={1} colEnd={4} color='gray'>
+                        {artistName}
+                    </GridItem>
+                    <GridItem rowStart={2} colStart={5} colEnd={5} margin='auto'>
+                        < AiOutlineHeart size='1.3rem'/>
+                    </GridItem>
+                    <GridItem rowStart={2} colSpan={1} colStart={6} color='gray' margin='auto'> 
+                        {formatTime(duration)}
+                    </GridItem>
+                    <GridItem rowStart={2} colStart={7} margin='auto'>
+                        <BsThreeDotsVertical size='1.3rem'/>
+                    </GridItem>
+                </Grid>
+            </Box>
+          
         </Box>
   )
 }

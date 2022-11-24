aa005a068e5de02bd63c657eadf671d82bc66448
Philip Ziolkowski <philipz4848@gmail.com>
2022-11-24 01:16:30 -0800

    Removing logs

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -5,12 +5,9 @@ const signedInPages = ['/favorites',]
 
 // runs before any page request
 export default function middleware(req) {
-    console.log('middleware hit')
-    console.log('request pathname: ', req.nextUrl.pathname)
 
     if (signedInPages.find((p) => p === req.nextUrl.pathname)) {
         const token = req.cookies.WAVES_ACCESS_TOKEN
-        console.log('token in middleware: ', token)
 
         if (!token) {
             return NextResponse.redirect('/signin')
